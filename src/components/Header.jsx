85f84dce3ece5da66824b9fbf8d4e9a7cfc1c62e
Ruppy <rupeshk7778@gmail.com>
2024-08-26 16:09:28 +0530

    Added illustration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import '../App.css';
+import React from "react";
+import "../App.css";
+import { Tooltip } from "react-tooltip";
 
 const Header = () => {
-    return (
-        <header className='flex flex-grow items-center h-1 justify-center'>
-          <h1 className='header-name text-4xl md:text-7xl'>RUPESH</h1>
-        </header>
-      )
-}
+  const ruppy = "/ruppy.png";
+  const jojoInfo =
+    "An illustration of me by my friend Jojo. Click to know more.";
+  const jojoLink = "https://www.instagram.com/jo.joart";
 
-export default Header
+  const handleImageClick = () => {
+    window.open(jojoLink, '_blank');
+  }
+
+  return (
+    <header className="flex flex-row items-center justify-center">
+      <img
+        data-tooltip-id="1"
+        data-tooltip-content={jojoInfo}
+        src={ruppy}
+        alt="ruppy"
+        className="w-[8%] cursor-pointer"
+        onClick={handleImageClick}
+      />
+      <Tooltip className="skill-tt text-xs text-white" id="1" />
+      <h1 className="header-name text-4xl md:text-7xl">RUPESH</h1>
+    </header>
+  );
+};
+
+export default Header;
